6900fe95dc338c22d5b96fd027c6d5f7f16a892b
Aidan Tan <theonebelowall701@gmail.com>
2026-03-10 15:42:08 +0800

    Add purpose comment for Pi Camera functionality test
    Added a comment to explain the purpose of the code.

--- cameraTest.py
+++ cameraTest.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 import time
 from picamera2 import Picamera2, Preview
 
+#Purpose of this code it is to test out the functionality of Pi Camera
+
 picam2=Picamera2()
 picam2.start_preview(Preview.QTGL)
 preview_config=picam2.create_preview_configuration()
@@ -9,3 +11,4 @@ picam2.configure(preview_config)
 
 picam2.start()
 time.sleep(2)
+
