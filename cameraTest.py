389af75330f7290626128f0ffe665adfd7f2c6f8
Aidan Tan <theonebelowall701@gmail.com>
2026-03-10 15:40:02 +0800

    Add files via upload

---
+++ cameraTest.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+from picamera2 import Picamera2, Preview
+
+picam2=Picamera2()
+picam2.start_preview(Preview.QTGL)
+preview_config=picam2.create_preview_configuration()
+picam2.configure(preview_config)
+
+picam2.start()
+time.sleep(2)
